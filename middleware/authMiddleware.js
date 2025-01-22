ea89fcc5789ae6fbb2749773a309110c81d68d54
Ricardo Lafontant <jrl9298@gmail.com>
2025-01-22 14:19:22 -0500

    attach userId to req

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,11 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
   // Extract the token from the Authorization header
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
-    return res.status(401).json({ message: 'No token, authorization denied' });
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   try {
@@ -13,10 +13,10 @@ const authenticate = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     // Add the decoded user data (userId) to the request object
-    req.user = decoded;
-    next();  // Proceed to the next middleware/controller
+    req.user = { id: decoded.userId };
+    next(); // Proceed to the next middleware/controller
   } catch (err) {
-    res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
 
