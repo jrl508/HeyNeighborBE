12d89a607678967278f634f0596080ca7645bcd1
Ricardo Lafontant <jrl9298@gmail.com>
2025-05-14 13:03:18 -0400

    added upload middleware

--- routes/toolRoutes.js
+++ routes/toolRoutes.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const ToolController = require("../controllers/toolController");
 const authMiddleware = require("../middleware/authMiddleware");
+const upload = require("../middleware/fileUploadMiddleware");
 
-router.post("/", authMiddleware, ToolController.createTool); // Add a new tool
+router.post(
+  "/",
+  authMiddleware,
+  upload.single("tool_image"),
+  ToolController.createTool
+); // Add a new tool
 router.get("/", authMiddleware, ToolController.getUserTools); // Get tools for the logged-in user
 router.put("/:id", authMiddleware, ToolController.updateTool); // Update a tool
 router.delete("/:id", authMiddleware, ToolController.deleteTool); // Delete a tool
