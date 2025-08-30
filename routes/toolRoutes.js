f416ef2e37fe58324cbc95aa8488f321c418a30d
Ricardo Lafontant <jrl9298@gmail.com>
2025-08-29 23:54:26 -0400

    changed updateTool call from put to patch

--- routes/toolRoutes.js
+++ routes/toolRoutes.js
@@ -11,7 +11,7 @@ router.post(
   ToolController.createTool
 ); // Add a new tool
 router.get("/", authMiddleware, ToolController.getUserTools); // Get tools for the logged-in user
-router.put("/:id", authMiddleware, ToolController.updateTool); // Update a tool
+router.patch("/:id", authMiddleware, ToolController.updateTool); // Update a tool
 router.delete("/:id", authMiddleware, ToolController.deleteTool); // Delete a tool
 
 module.exports = router;
