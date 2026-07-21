9ffb947d7fc15b846e47f9cdb0653c3aca475196
Ricardo Lafontant <jrl9298@gmail.com>
2026-07-21 02:03:37 -0400

    feat(db): update knexfile and db.js to support DATABASE_URL, NODE_ENV and SSL

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,33 @@ require("dotenv").config(); // Load environment variables
 module.exports = {
   development: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+    connection: process.env.DATABASE_URL
+      ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+      : {
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+        },
+    migrations: {
+      directory: "./database/migrations",
     },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: { min: 2, max: 10 },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL
+      ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+      : {
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          ssl: { rejectUnauthorized: false },
+        },
     migrations: {
       directory: "./database/migrations",
     },
@@ -18,3 +39,4 @@ module.exports = {
     pool: { min: 2, max: 10 },
   },
 };
+
